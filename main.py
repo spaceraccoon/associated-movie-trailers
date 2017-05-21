42939533e26c44d73bff461c0a21777bff67a89f
EUgene Lim <eugene@debian>
2017-05-21 08:02:02 -0400

    PEP8 compliance

--- main.py
+++ main.py
@@ -3,46 +3,50 @@ import tmdbsimple as tmdb
 import fresh_tomatoes
 import wikipedia
 
-tmdb.API_KEY = '65abd8a5a458bd5b2f1fa71446e60c32'	# Insert your API key here
-PERSON = 'Ang Lee'	# Insert your person (actor, director, etc) here
+tmdb.API_KEY = '65abd8a5a458bd5b2f1fa71446e60c32'  # Insert your API key here
+PERSON = 'Ang Lee'  # Insert your person (actor, director, etc) here
 movies = []
 
 # Search tmdb for person
 search = tmdb.Search()
-search.person(query = PERSON)
+search.person(query=PERSON)
 
 if search.results:
-	result = search.results[0]	# Selects first, most relevant result
-	name = result['name']
-	person = Person(name,
-					'http://image.tmdb.org/t/p/w342' + result['profile_path'],
-					wikipedia.summary(name).encode('utf-8'))	# Ecode in utf-8 for special characters or other languages
-	associated_movies = result['known_for']		# Picks top movies associated with person
+    result = search.results[0]  # Selects first, most relevant result
+    name = result['name']
+    person = Person(name,
+                    'http://image.tmdb.org/t/p/w342' + result['profile_path'],
+                    # Ecode in utf-8 for special characters or other languages
+                    wikipedia.summary(name).encode('utf-8'))
+    # Picks top movies associated with person
+    associated_movies = result['known_for']
 
-	for movie in associated_movies:
-		if movie['media_type'] == 'movie':	# Excludes non-movie media like trailers
-			title = movie['title']
-			image = 'http://image.tmdb.org/t/p/w342' + movie['poster_path']
+    for movie in associated_movies:
+        # Excludes non-movie media like trailers
+        if movie['media_type'] == 'movie':
+            title = movie['title']
+            image = 'http://image.tmdb.org/t/p/w342' + movie['poster_path']
 
-			trailer = None
-			identity = tmdb.Movies(movie['id'])
-			videos = identity.videos()['results']	# Acquires associated videos of selected movie
+            trailer = None
+            identity = tmdb.Movies(movie['id'])
+            # Acquires associated videos of selected movie
+            videos = identity.videos()['results']
 
-			if videos:
-				for video in videos:
-					
-					if video['type'] == 'Trailer':	# Excludes non-trailer media like behind-the-scenes
-						trailer = video['key']	# Acquires Youtube ID
-						break	# Ensures that first, most relevant result is selected
-					else:
-						continue	# Skips video if not trailer
-			
-			if not trailer:
-				continue	# Skips movie if no trailer found
+            if videos:
+                for video in videos:
+                    # Excludes non-trailer media like behind-the-scenes
+                    if video['type'] == 'Trailer':
+                        trailer = video['key']  # Acquires Youtube ID
+                        break  # Selects first, most relevant result
+                    else:
+                        continue  # Skips video if not trailer
 
-			movies.append(Video(title, image, trailer))
-	# Outputs formatted page
-	fresh_tomatoes.open_movies_page(movies, person)
+            if not trailer:
+                continue  # Skips movie if no trailer found
+
+            movies.append(Video(title, image, trailer))
+    # Outputs formatted page
+    fresh_tomatoes.open_movies_page(movies, person)
 else:
-	# If no results for search, output error page
-	fresh_tomatoes.error_page()
+    # If no results for search, output error page
+    fresh_tomatoes.error_page()
--- media.py
+++ media.py
@@ -1,11 +1,12 @@
 class Media():
-	def __init__(self, title, image):
-		self.title = title
-		self.poster_image_url = image
+    def __init__(self, title, image):
+        self.title = title
+        self.poster_image_url = image
+
 
 class Video(Media):
-	"""Video class for movies.
-    
+    """Video class for movies.
+
     Atributes:
         title (str): Title of movie.
         image (str): URL path to poster image.
@@ -13,19 +14,20 @@ class Video(Media):
 
     """
 
-	def __init__(self, title, image, trailer):
-		Media.__init__(self, title, image)
-		self.trailer_youtube_url = trailer
+    def __init__(self, title, image, trailer):
+        Media.__init__(self, title, image)
+        self.trailer_youtube_url = trailer
+
 
 class Person(Media):
-	"""Person class for people.
-    
+    """Person class for people.
+
     Atributes:
         title (str): Name of person.
         image (str): URL path to profile  image.
         bio (str): Bio of person.
 
     """
-	def __init__(self, title, image, bio):
-		Media.__init__(self, title, image)
-		self.bio = bio
+    def __init__(self, title, image, bio):
+        Media.__init__(self, title, image)
+        self.bio = bio
